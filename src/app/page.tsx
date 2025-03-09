e0a532c1f5232781ab7079e5d5f4b18daf6d9cd4
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-03-09 23:23:26 +0800

    feat: create new page file routing for posts and styled header navigation between posts and todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h2>Next App</h2>
+    <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex items-center justify-between">
+        <h2 className="text-3xl font-bold">Next App</h2>
+        <div className="flex items-center gap-3">
+          <Link href="/posts" className="text-indigo-600 text-2xl font-bolder">
+            Posts
+          </Link>
+          <span className="text-2xl font-bold">|</span>
+          <Link href="/todos" className="text-indigo-600 text-2xl font-bolder">
+            Todos
+          </Link>
+        </div>
       </main>
     </div>
   );
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,17 +1,10 @@
 import { getTodo } from "@/app/data-access/todo";
 
-export default async function Todos({
-  params,
-}: {
-  params: { todoId: number };
-}) {
-  const todo = await getTodo(params.todoId);
-  console.log("todo", todo);
+export default async function Posts() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex items-center justify-between">
-        <h2 className="text-3xl font-bold">Selected Todo</h2>
-        <p>{todo?.title}</p>
+        <h2 className="text-3xl font-bold">Posts</h2>
       </main>
     </div>
   );
