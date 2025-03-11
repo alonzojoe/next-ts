9a531cd02dabbff22cdc3d1078a8cd66bda9ef3e
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-11 08:31:28 +0800

    feat: display selected todo and post details

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -11,11 +11,12 @@ export default async function Posts({ params }: Params) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center">
-        <h2 className="text-3xl font-bold mb-5">Posts</h2>
+        <h2 className="text-3xl font-bold mb-5">Selected Post</h2>
         {post ? (
           <div>
             <div key={post.id}>
               {post.id}. <span>{post.title.slice(0, 50)}...</span>
+              <p>{post.body}</p>
             </div>
           </div>
         ) : (
--- src/app/todos/[todoId]/page.tsx
+++ src/app/todos/[todoId]/page.tsx
@@ -9,9 +9,20 @@ export default async function Todos({
   console.log("todo", todo);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex items-center justify-between">
-        <h2 className="text-3xl font-bold">Selected Todo</h2>
-        <p>{todo?.title}</p>
+      <main className="flex flex-col items-center">
+        <h2 className="text-3xl font-bold mb-5">Selected Todo</h2>
+        {todo ? (
+          <>
+            <p>
+              {todo.id}. {todo.title}{" "}
+              <span className="font-bold">
+                Status: {todo.completed ? "Completed" : "Pending"}
+              </span>
+            </p>
+          </>
+        ) : (
+          <p>Todo not found</p>
+        )}
       </main>
     </div>
   );
